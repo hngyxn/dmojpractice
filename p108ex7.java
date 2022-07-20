40538209f9535ba882f9868ff78872ebac6f6cae
Rita Huang <ritahuang.ca@gmail.com>
2022-07-20 19:37:30 -0400

    commit

--- p108ex7.java
+++ p108ex7.java
@@ -1,17 +1,23 @@
 import java.util.*;
 import java.io.*;
-public class p100ex4 {
+public class p108ex7 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	public static void main(String[] args) throws IOException{
-		String str = readLine();
-        if (str.equals("MG")) System.out.println("midget girls");
-        else if (str.equals("MB")) System.out.println("midget boys");
-        else if (str.equals("JG")) System.out.println("junior girls");
-        else if (str.equals("JB")) System.out.println("junior boys");
-        else if (str.equals("SG")) System.out.println("senior girls");
-        else if (str.equals("SB")) System.out.println("senior boys");
-        else System.out.println("invalid code");
+		int t = readInt(), b = 100, count = 0;
+        for (int i = 0; i < t; i++) {
+            int a = readInt();
+            if (a <= 30) System.out.println(38);
+            else if (a > 30 && a <= 50) System.out.println(55);
+            else if (a > 50 && a <= 100) System.out.println(73);
+            else {
+                while (b < a) {
+                    b += 50;
+                    count++;
+                }
+                System.out.println(73 + (24*count));
+            }
+        }
 	}
 	static String next () throws IOException {
 		while (st == null || !st.hasMoreTokens())
