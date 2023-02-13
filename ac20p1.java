0bb5b3ce839ba494d947641595edb721faf89ac6
Rita Huang <ritahuang.ca@gmail.com>
2023-02-12 21:49:51 -0500

    commit

--- ac20p1.java
+++ ac20p1.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
-@SuppressWarnings("resource")
-
+import java.util.*;
+import java.io.*;
 public class ac20p1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[] args) throws IOException{
+		int t = readInt();
         for (int i = 0; i < t; i++) {
-            long a = sc.nextLong(), b = sc.nextLong(), c = sc.nextLong();
+            long a = readLong(), b = readLong(), c = readLong();
 
             long min = Math.min(a, Math.min(b, c));
             long max = Math.max(a, Math.max(b, c));
@@ -20,5 +20,25 @@ public class ac20p1 {
                 System.out.println("O");
             }
         }
-     }
+	}
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static long readLong () throws IOException {
+		return Long.parseLong(next());
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+	static double readDouble () throws IOException {
+		return Double.parseDouble(next());
+	}
+	static char readCharacter () throws IOException {
+		return next().charAt(0);
+	}
+	static String readLine () throws IOException {
+		return br.readLine().trim();
+	}
 }
